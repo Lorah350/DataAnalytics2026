950d442ace8ba0d31d9176b9de65753700cfcfc6
Lorah350 <lorahsamiel@gmail.com>
2026-04-15 00:40:41 -0400

    Added queries of actor and film tables

--- week-02/Ex1B_first_query.sql
+++ week-02/Ex1B_first_query.sql
@@ -1,3 +1,11 @@
  /*
  sakila is a data base used to collect information about films and actors for use by students.
 /*
+USE Sakila;
+SELECT * FROM actor; -- Retrieved [200] records
+SELECT * FROM film; -- Retrieved [1000] records
+
+
+
+
+
