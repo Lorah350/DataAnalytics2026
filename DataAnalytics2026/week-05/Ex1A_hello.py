e091ca0e464e358d847d9e148e9332ede73887eb
Lorah350 <lorahsamiel@gmail.com>
2026-05-05 06:45:53 -0400

    Add reserved words and keyword definitions to Ex1B

---
+++ DataAnalytics2026/week-05/Ex1A_hello.py
@@ -0,0 +1,35 @@
+# My python program
+print('Hello world!')
+
+#My variable
+message = 'Hello world'
+print (message)
+
+#Why does it print twice?
+#Because the first print shows the text directly,
+#the second print()prints the variable that contains the same text.
+
+#Displaying variables for dollars and cents
+dollars = 3
+cents = 0.50
+print(dollars + cents)
+
+#19:observation
+#The result is 3.5 instead of 3.50 because python removes unneccessary zeros.
+#when displaying floats.
+
+#20:Update cents and print again
+cents = cents + 0.25
+print(dollars + cents)
+
+#21:Combine strings
+d_str = "3 dollars"
+c_str = "50 cents"
+print(d_str +" " + c_str)
+
+
+
+
+
+
+
--- DataAnalytics2026/week-05/hello.py
+++
@@ -1,6 +0,0 @@
-print('Hello world!')
-message = 'Hello world'
-print (message)
-
-
-
