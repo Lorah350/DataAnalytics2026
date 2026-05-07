b643dbcf820c2d210b3dcaa7af31257d38cc72e5
Lorah350 <lorahsamiel@gmail.com>
2026-05-07 11:35:21 -0400

    commit changes

---
+++ DataAnalytics2026/week-05/Area_of _rectangle.py
@@ -0,0 +1,12 @@
+
+# Birthday dimensions
+side_a = 5   # Example: May
+side_b = 12  # Example: 12th
+
+# Calculate area
+area = side_a * side_b
+
+# Display results
+print("Side A is " + str(side_a))
+print("Side B is " + str(side_b))
+print("The area of the rectangle is " + str(area))
