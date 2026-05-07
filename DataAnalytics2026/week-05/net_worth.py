1b67f2724da33fae89ec067df0fdfffeeb1826c1
Lorah350 <lorahsamiel@gmail.com>
2026-05-07 06:11:18 -0400

    Saved Ex2A_MathScripts with all calculations completed

---
+++ DataAnalytics2026/week-05/net_worth.py
@@ -0,0 +1,19 @@
+# Define assets
+cash = 1500
+car_value = 8000
+savings = 2500
+
+# Define debts
+credit_card = 1200
+student_loans = 3000
+
+# Calculate totals
+total_assets = cash + car_value + savings
+total_debts = credit_card + student_loans
+net_worth = total_assets - total_debts
+
+# Display results
+print("Your total assets are " + str(total_assets))
+print("Your total debts are " + str(total_debts))
+print("Your net worth is " + str(net_worth))
+
