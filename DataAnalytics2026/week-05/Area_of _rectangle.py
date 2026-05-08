f17541d3cd4faec6f4377bbe0932c10704fa01c4
Lorah350 <lorahsamiel@gmail.com>
2026-05-07 20:35:59 -0400

    commit changes made to Ex2A_MathScripts

--- DataAnalytics2026/week-05/Area_of _rectangle.py
+++ DataAnalytics2026/week-05/Area_of _rectangle.py
@@ -10,3 +10,16 @@ area = side_a * side_b
 print("Side A is " + str(side_a))
 print("Side B is " + str(side_b))
 print("The area of the rectangle is " + str(area))
+
+
+# Formular of Tip Amount = Bill × Tip Percentage
+
+# Example bill and tip percentage
+bill = 45.50
+tip_percent = 0.18  # 18%
+
+# Calculate tip
+tip_amount = bill * tip_percent
+
+# Display results
+print("The tip on a $" + str(bill) + " restaurant bill is $" + format(tip_amount, ".2f"))
